710f6d452286a7df841ee50b3d8f0b69f73ad1ef
Clemens Czurda <clemens.czurda@gmail.com>
2022-07-07 14:00:54 +0200

    Kleine Web-App ohne Session

--- index.js
+++ index.js
@@ -2,11 +2,32 @@
 const express = require("express")
 const app = express()
 
-app.use(express.static('public'));
+const posts = [
+    {title: 'Title 1', body: 'Body 1' },
+    {title: 'Title 2', body: 'Body 2' },
+    {title: 'Title 3', body: 'Body 3' },
+    {title: 'Title 4', body: 'Body 4' },
+];
 
-// Handling GET /hello request
-app.get("/hello", (req, res, next) => {
-	res.send("This is the hello response");
+const user = {
+    firstName: 'Clemens',
+    lastName: 'Czurda',
+	admin: false
+};
+
+//app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('pages/index', { user: user, title: "Startseite"})
+})
+
+app.get('/articles', (req, res) => {
+    res.render('pages/articles', { articles: posts, title: "Articles"})
+})
+
+app.get('/about', (req, res) => {
+    res.render('pages/about', { articles: posts, title: "About"})
 })
 
 // Server setup
