e06a9eb0348d68b36f6d4d4acf6e29620472f711
Clemens Czurda <clemens.czurda@gmail.com>
2022-07-07 15:22:22 +0200

    Ein kleiner Login ohne Passwort

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 // Importing express module
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+
+// creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
+
+//session middleware
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
+
+// parsing the incoming data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 const posts = [
     {title: 'Title 1', body: 'Body 1' },
@@ -9,26 +27,49 @@ const posts = [
     {title: 'Title 4', body: 'Body 4' },
 ];
 
-const user = {
-    firstName: 'Clemens',
-    lastName: 'Czurda',
-	admin: false
-};
+const users = {
+	"cczurda": {firstName: 'Clemens', lastName: 'Czurda', admin: true}, 
+	"fabio": {firstName: 'Fabio', lastName: 'Vogler', admin: false}
+}
 
 //app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('pages/index', { user: user, title: "Startseite"})
+    res.render('pages/index', { title: "Startseite", session: req.session})
 })
 
 app.get('/articles', (req, res) => {
-    res.render('pages/articles', { articles: posts, title: "Articles"})
+    res.render('pages/articles', { articles: posts, title: "Articles", session: req.session})
 })
 
 app.get('/about', (req, res) => {
-    res.render('pages/about', { articles: posts, title: "About"})
+    res.render('pages/about', { title: "About", session: req.session})
 })
+
+app.get('/login', (req, res) => {
+    res.render('login', { title: "Login", session: req.session})
+})
+
+app.get('/logout', (req, res) => {
+	req.session.destroy();
+    res.redirect('/');
+})
+
+app.post('/user',(req,res) => {
+    //if(req.body.username == myusername && req.body.password == mypassword){
+        session=req.session;
+        session.userid=req.body.username;
+		session.user = users[req.body.username];
+        console.log(req.session)
+        res.redirect('/');
+    //}
+    //else{
+    //    res.send('Invalid username or password');
+    //	}
+})
+
+app.use(express.static('public'));
 
 // Server setup
 app.listen(3000, () => {
